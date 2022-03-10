2f22c5ed8b120f5cf0ad5406b5143640695f611b
tihelena <elenatikhon@yandex.ru>
2022-03-10 04:01:54 +0300

    create new project

---
+++ src/test/java/ru/gb/MyFirstTest.java
@@ -0,0 +1,51 @@
+package ru.gb;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.net.URL;
+
+public class MyFirstTest {
+
+    @Test
+    public void checkEmptyEmail() throws Exception {
+
+        // Устанавливаем capabilities.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+//      Проверьте актуальность пути до приложения на своём компьютере.
+        capabilities.setCapability("app", "C:\\Users\\NUC J3455\\Downloads\\Android-NativeDemoApp-0.2.1.apk");
+//        Пример для предустановленного приложения.
+//        capabilities.setCapability("appPackage", "com.google.android.apps.maps");
+//        capabilities.setCapability("appActivity", "com.google.android.maps.MapsActivity");
+//        capabilities.setCapability("noReset", true);
+
+//      Устанавливаем и открываем приложение.
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(2000);
+        //      Нажимаем Login в меню
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+//      Нажимаем Login на странице логин/пароль.
+        MobileElement loginButton = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
+        loginButton.click();
+        Thread.sleep(2000);
+//      Проверяем текст ошибки.
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+        Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+    }
+
+
+
+
+}
+
---
+++ src/test/java/ru/gb/MyThirdTest.java
@@ -0,0 +1,73 @@
+package ru.gb;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+public class MyThirdTest {
+    //регистрация: валидный email и пароль
+
+    @Test
+    public void checkValidSignUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("unicodeKeyboard",true);
+
+        capabilities.setCapability("app", "C:\\Users\\NUC J3455\\Downloads\\Android-NativeDemoApp-0.2.1.apk");
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        //tap Login
+        Thread.sleep(2000);
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+
+        //tap sign up
+        MobileElement signUp = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"button-sign-up-container\"]/android.view.ViewGroup/android.widget.TextView");
+        signUp.click();
+        Thread.sleep(2000);
+
+        //input email
+        MobileElement inputEmail = (MobileElement) driver.findElementByAccessibilityId("input-email");
+        inputEmail.click();
+        inputEmail.sendKeys("elena@mail.ru");
+        Thread.sleep(2000);
+
+        //input password
+        MobileElement inputPassword = (MobileElement) driver.findElementByAccessibilityId("input-password");
+        inputPassword.click();
+        inputPassword.sendKeys("qwerty12345");
+        Thread.sleep(2000);
+
+        //confirm password
+        MobileElement inputRepeatPassword = (MobileElement) driver.findElementByAccessibilityId("input-repeat-password");
+        inputRepeatPassword.click();
+        inputRepeatPassword.sendKeys("qwerty12345");
+        Thread.sleep(2000);
+
+        //submit
+        ((AndroidDriver)driver).pressKey(new KeyEvent(AndroidKey.ENTER));
+        Thread.sleep(2000);
+
+        //assert that successfully signed up
+        MobileElement message = (MobileElement) driver.findElementById("android:id/message");
+        message.click();
+        Assert.assertEquals(message.getText(), "You successfully signed up!");
+
+
+
+    }
+}
