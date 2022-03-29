dbeaf3dd575dc22d507e2f7d957b7d65b409635b
elenatihonova <elenatikhon@yandex.ru>
2022-03-29 13:42:58 +0300

    lesson3_grid

--- src/test/java/ru/gb/base/BaseTest.java
+++ src/test/java/ru/gb/base/BaseTest.java
@@ -1,6 +1,7 @@
 package ru.gb.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebDriver;
@@ -11,49 +12,53 @@ import ru.gb.pages.MainPage;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static com.codeborne.selenide.Selenide.close;
-
 // базовый класс для всех будущих тестов
+// базовый класс для всех будующий тестов.
 public class BaseTest {
 
-    // метод, который будет открывать наше приложение на телефоне
-    public MainPage openApp() {
+    // метод который будет открывать наше приложение на телефоне.
+    // добавили значение String device
+    public MainPage openApp(String device) {
         WebDriver driver = null;
         try {
-            driver = getAndroidDriver();
+            // передаем значение device в создание драйвера
+            driver = getAndroidDriver(device);
         } catch (MalformedURLException e) {
             e.printStackTrace();
             System.out.println("Opps, we have problems with URL for driver!");
         }
-        // устанавливаем драйвер для selenide
+        // устанавливаем драйвер для selenide.
         WebDriverRunner.setWebDriver(driver);
-        // возвращаем главную страницу для будущей работы с ней в тесте
+        // возвращаем главную страницу для будущей работы с ней в тесте.
         return new MainPage();
-    }
-
-    public static WebDriver getAndroidDriver() throws MalformedURLException {
-        // устанавливаем capabilities
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Pixel");
-        capabilities.setCapability("platformVersion", "10");
-        capabilities.setCapability("udid", "emulator-5554");
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("app", "/Users/elenatihonova/Downloads/Android-NativeDemoApp-0.2.1.apk");
-        // папка для сохранения скриншотов selenide
-        Configuration.reportsFolder = "screenshots/actual";
-        // устанавливаем и открываем приложение
-        return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @AfterClass
     public void setDown() {
-        close();
+        Selenide.close();
+    }
+
+    // добавили значение String device
+    public  AndroidDriver getAndroidDriver(String device) throws MalformedURLException {
+        // устанавливаем capabilities.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        // указываем для appium на каком девайсе хотим запускать тест.
+        switch (device){
+            case "pixel 10":
+                capabilities.setCapability("udid", "emulator-5554");
+                break;
+            case "pixel 11":
+                capabilities.setCapability("udid", "emulator-5556");
+                break;
+        }
+        capabilities.setCapability("app", "/Users/elenatihonova/Downloads/Android-NativeDemoApp-0.2.1.apk");
+
+        // папка для сохранения скриншотов selenide.
+        Configuration.reportsFolder = "screenshots/actual";
+
+        // устанавливаем и открываем приложение. !!! Указываем URL Grid !!!
+        return new AndroidDriver(new URL("http://0.0.0.0:4444/wd/hub"), capabilities);
     }
 }
-
-
-
-
-
